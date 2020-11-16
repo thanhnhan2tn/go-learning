3c5e28b05b16a342689f481afaa4a163f424d3cb
Thanh Nhan <thanhnhan2tn@gmail.com>
2020-11-16 19:18:04 +0700

    Update main.go

--- main.go
+++ main.go
@@ -9,5 +9,7 @@ func main() {
 			"message": "pong",
 		})
 	})
-	require.NoError(t, r.Run()) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	if err := r.Run(); err != nil {
+		panic(err)
+	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
